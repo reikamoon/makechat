97570065d66d72f4522c40696734cc24d2409032
Anjelica <ambrosio.anjelica@gmail.com>
2020-12-01 14:13:58 -0800

    users can now log in

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //app.js
+
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
@@ -6,12 +7,16 @@ const server = require('http').Server(app);
 //Socket.io
 const io = require('socket.io')(server);
 io.on("connection", (socket) => {
-  console.log("🔌 New user connected! 🔌");
+  // This file will be read on new socket connections
+  require('./sockets/chat.js')(io, socket);
 })
 
+//Express View Engine for Handlebars
 const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+//Establish your public folder
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index.handlebars');
